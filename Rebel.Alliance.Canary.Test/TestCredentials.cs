f327d5262f99442e15db50a1208b402198b9360d
dlandi <dennis@dennislandi.com>
2024-09-13 17:11:39 -0400

    Updated XUnit tests

--- Rebel.Alliance.Canary.Test/TestCredentials.cs
+++ Rebel.Alliance.Canary.Test/TestCredentials.cs
@@ -13,9 +13,12 @@ using Rebel.Alliance.Canary.Configuration;
 using Rebel.Alliance.Canary.InMemoryActorFramework.Actors.CredentialVerifierActor;
 using Rebel.Alliance.Canary.Actor.Interfaces.Actors;
 using Rebel.Alliance.Canary.InMemoryActorFramework.Actors.OIDCClientActor;
-using Rebel.Alliance.Canary.InMemoryActorFramework.ActorSystem;
-using Rebel.Alliance.Canary.Actor.Interfaces;
+using Rebel.Alliance.Canary.InMemoryActorFramework.Actors.VerifiableCredentialActor;
+using Rebel.Alliance.Canary.InMemoryActorFramework.Actors.VerifiableCredentialAsRootOfTrustActor;
+using Rebel.Alliance.Canary.InMemoryActorFramework.Actors.TrustFrameworkManagerActor;
+using Rebel.Alliance.Canary.InMemoryActorFramework.Actors.RevocationManagerActor;
 using Rebel.Alliance.Canary.VerifiableCredentials.Messaging;
+using Rebel.Alliance.Canary.InMemoryActorFramework.ActorSystem;
 
 namespace Rebel.Alliance.Canary.Tests
 {
@@ -24,12 +27,98 @@ namespace Rebel.Alliance.Canary.Tests
         private readonly IDecentralizedOIDCProviderService _oidcProviderService;
         private readonly ICredentialVerifierActor _credentialVerifierActor;
         private readonly IOIDCClientActor _oidcClientActor;
+        private readonly IVerifiableCredentialActor _verifiableCredentialActor;
+        private readonly IVerifiableCredentialAsRootOfTrustActor _verifiableCredentialAsRootOfTrustActor;
+        private readonly ITrustFrameworkManagerActor _trustFrameworkManagerActor;
+        private readonly IRevocationManagerActor _revocationManagerActor;
 
         public CanaryOidcTests(CanaryTestFixture fixture)
         {
             _oidcProviderService = fixture.OidcProviderService;
             _credentialVerifierActor = fixture.CredentialVerifierActor;
             _oidcClientActor = fixture.OidcClientActor;
+            _verifiableCredentialActor = fixture.VerifiableCredentialActor;
+            _verifiableCredentialAsRootOfTrustActor = fixture.VerifiableCredentialAsRootOfTrustActor;
+            _trustFrameworkManagerActor = fixture.TrustFrameworkManagerActor;
+            _revocationManagerActor = fixture.RevocationManagerActor;
+        }
+
+        [Fact]
+        public async Task CreateAndVerifyRootCredential_ShouldSucceed()
+        {
+            var issuerId = "root_issuer";
+            var claims = new Dictionary<string, string> { { "type", "root" } };
+            var masterKeyId = "master_key_1";
+
+            var createRootMsg = new CreateRootCredentialMessage(issuerId, claims, masterKeyId);
+            var rootCredential = await _verifiableCredentialAsRootOfTrustActor.CreateRootCredentialAsync(issuerId, claims, masterKeyId);
+
+            Assert.NotNull(rootCredential);
+            Assert.Equal(issuerId, rootCredential.Issuer);
+
+            var verifyChainMsg = new VerifyCredentialChainMessage(rootCredential, rootCredential);
+            var isValid = await _verifiableCredentialAsRootOfTrustActor.VerifyCredentialChainAsync(rootCredential, rootCredential);
+
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public async Task IssueAndVerifySubordinateCredential_ShouldSucceed()
+        {
+            // First, create a root credential
+            var rootIssuerId = "root_issuer";
+            var rootClaims = new Dictionary<string, string> { { "type", "root" } };
+            var masterKeyId = "master_key_1";
+
+            var rootCredential = await _verifiableCredentialAsRootOfTrustActor.CreateRootCredentialAsync(rootIssuerId, rootClaims, masterKeyId);
+
+            // Now, issue a subordinate credential
+            var subIssuerId = "sub_issuer";
+            var subClaims = new Dictionary<string, string> { { "type", "subordinate" } };
+            var derivedKeyId = "derived_key_1";
+
+            var issueSubMsg = new IssueSubordinateCredentialMessage(subIssuerId, rootCredential, subClaims, derivedKeyId);
+            var subCredential = await _verifiableCredentialAsRootOfTrustActor.IssueSubordinateCredentialAsync(subIssuerId, rootCredential, subClaims, derivedKeyId);
+
+            Assert.NotNull(subCredential);
+            Assert.Equal(subIssuerId, subCredential.Issuer);
+            Assert.Equal(rootCredential.Id, subCredential.ParentCredentialId);
+
+            var verifyChainMsg = new VerifyCredentialChainMessage(subCredential, rootCredential);
+            var isValid = await _verifiableCredentialAsRootOfTrustActor.VerifyCredentialChainAsync(subCredential, rootCredential);
+
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public async Task RevokeCredential_ShouldInvalidateCredential()
+        {
+            var credential = CredentialGenerator.GenerateUserCredential("user123", new Dictionary<string, string>());
+
+            var revokeMsg = new RevokeCredentialMessage(credential.Id);
+            await _revocationManagerActor.RevokeCredentialAsync(credential.Id);
+
+            var isRevokedMsg = new IsCredentialRevokedMessage(credential.Id);
+            var isRevoked = await _revocationManagerActor.IsCredentialRevokedAsync(credential.Id);
+
+            Assert.True(isRevoked);
+        }
+
+        [Fact]
+        public async Task RegisterAndVerifyIssuer_ShouldSucceed()
+        {
+            var issuerId = "test_issuer";
+            var publicKey = "test_public_key";
+
+            var registerMsg = new RegisterIssuerMessage(issuerId, publicKey);
+            var isRegistered = await _trustFrameworkManagerActor.RegisterIssuerAsync(issuerId, publicKey);
+
+            Assert.True(isRegistered);
+
+            var isTrustedMsg = new IsTrustedIssuerMessage(issuerId);
+            var isTrusted = await _trustFrameworkManagerActor.IsTrustedIssuerAsync(issuerId);
+
+            Assert.True(isTrusted);
         }
 
         [Fact]
@@ -133,11 +222,16 @@ namespace Rebel.Alliance.Canary.Tests
         }
     }
 
+
     public class CanaryTestFixture : IDisposable
     {
         public IDecentralizedOIDCProviderService OidcProviderService { get; }
         public ICredentialVerifierActor CredentialVerifierActor { get; }
         public IOIDCClientActor OidcClientActor { get; }
+        public IVerifiableCredentialActor VerifiableCredentialActor { get; }
+        public IVerifiableCredentialAsRootOfTrustActor VerifiableCredentialAsRootOfTrustActor { get; }
+        public ITrustFrameworkManagerActor TrustFrameworkManagerActor { get; }
+        public IRevocationManagerActor RevocationManagerActor { get; }
 
         public CanaryTestFixture()
         {
@@ -167,26 +261,23 @@ namespace Rebel.Alliance.Canary.Tests
             services.AddSingleton<IKeyStore, InMemoryKeyStore>();
             services.AddSingleton<IKeyManagementService, KeyManagementService>();
 
-            services.AddTransient<ICredentialVerifierActor>(sp => new CredentialVerifierActor(
-                Guid.NewGuid().ToString(),
-                sp.GetRequiredService<ICryptoService>(),
-                sp.GetRequiredService<IRevocationManagerActor>(),
-                sp.GetRequiredService<ILogger<CredentialVerifierActor>>(),
-                webAppVc
-            ));
-
-            services.AddTransient<IOIDCClientActor>(sp => new OIDCClientActor(
-                Guid.NewGuid().ToString(),
-                sp.GetRequiredService<IActorStateManager>(),
-                sp.GetRequiredService<IActorMessageBus>(),
-                sp.GetRequiredService<ILogger<OIDCClientActor>>()
-            ));
+            // Register all actor types
+            services.AddTransient<ICredentialVerifierActor, CredentialVerifierActor>();
+            services.AddTransient<IOIDCClientActor, OIDCClientActor>();
+            services.AddTransient<IVerifiableCredentialActor, VerifiableCredentialActor>();
+            services.AddTransient<IVerifiableCredentialAsRootOfTrustActor, VerifiableCredentialAsRootOfTrustActor>();
+            services.AddTransient<ITrustFrameworkManagerActor, TrustFrameworkManagerActor>();
+            services.AddTransient<IRevocationManagerActor, RevocationManagerActor>();
 
             var serviceProvider = services.BuildServiceProvider();
 
             OidcProviderService = serviceProvider.GetRequiredService<DecentralizedOIDCProvider>();
             CredentialVerifierActor = serviceProvider.GetRequiredService<ICredentialVerifierActor>();
             OidcClientActor = serviceProvider.GetRequiredService<IOIDCClientActor>();
+            VerifiableCredentialActor = serviceProvider.GetRequiredService<IVerifiableCredentialActor>();
+            VerifiableCredentialAsRootOfTrustActor = serviceProvider.GetRequiredService<IVerifiableCredentialAsRootOfTrustActor>();
+            TrustFrameworkManagerActor = serviceProvider.GetRequiredService<ITrustFrameworkManagerActor>();
+            RevocationManagerActor = serviceProvider.GetRequiredService<IRevocationManagerActor>();
         }
 
         public void Dispose()
